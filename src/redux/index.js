b7daf6fd6ce35fddaec0ea0140758af94eab60a3
Suhair Zain <suhairzain11@gmail.com>
2017-10-07 11:50:58 +0530

    Integrated redux-thunk and remote-redux-devtools

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,4 +1,6 @@
-import { combineReducers, createStore } from 'redux'
+import { applyMiddleware, combineReducers, createStore } from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'remote-redux-devtools'
 
 import user from '../components/UserInfo/reducer'
 
@@ -7,11 +9,10 @@ let store
 /* Would help us set up the store with initial values if the future if needed */
 export const setupStore = () => {
   if (!store) {
-    store = createStore(
-      combineReducers({
-        user
-      })
-    )
+    const rootReducer = combineReducers({
+      user
+    })
+    store = createStore(rootReducer, {}, composeWithDevTools(applyMiddleware(thunk)))
   }
   return store
 }
