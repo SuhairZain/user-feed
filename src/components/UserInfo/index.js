3aa67eca67ee7db3883c3ca76e15a1b06317b5e6
Suhair Zain <suhairzain11@gmail.com>
2017-10-07 11:58:30 +0530

    Made UserInfo component and fetches dummy data using redux-thunk

--- src/components/UserInfo/index.js
+++ src/components/UserInfo/index.js
@@ -1,26 +1,78 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
-import { Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 
 import { connect } from 'react-redux'
 
+import { getUserDetails } from './reducer'
+
+import { DIVIDER } from '../../colors'
+import Image from '../common/Image'
+import Text from '../common/Text'
+
 class UserInfo extends PureComponent {
   static propTypes = {
-    name: PropTypes.string.isRequired
+    bio: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    dispatch: PropTypes.func.isRequired
+  }
+
+  componentDidMount() {
+    this.props.dispatch(getUserDetails())
   }
 
   render() {
-    const { name } = this.props
+    const { bio, image, name } = this.props
     return (
-      <View>
-        <Text>{name}</Text>
+      <View style={styles.container}>
+        <Image resizeMode="cover" source={{ uri: image }} style={styles.thumbnail} />
+        <View style={styles.texts}>
+          <Text style={styles.name}>{name}</Text>
+          <Text numberOfLines={3} style={styles.bio}>
+            {bio}
+          </Text>
+        </View>
       </View>
     )
   }
 }
 
-const mapStateToProps = state => ({
-  name: state.user.name
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    borderBottomColor: DIVIDER,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    flexDirection: 'row',
+    paddingHorizontal: 16,
+    paddingVertical: 16
+  },
+  thumbnail: {
+    borderRadius: 50,
+    height: 100,
+    marginRight: 24,
+    width: 100
+  },
+  texts: {
+    flex: 1
+  },
+  name: {
+    fontSize: 20,
+    marginBottom: 8,
+    width: 160
+  },
+  bio: {
+    fontSize: 16
+  }
 })
+
+const mapStateToProps = state => {
+  const { bio, name, profileThumbnail } = state.user.data
+  return {
+    bio,
+    image: profileThumbnail,
+    name
+  }
+}
 
 export default connect(mapStateToProps)(UserInfo)
--- src/components/UserInfo/reducer.js
+++ src/components/UserInfo/reducer.js
@@ -1,9 +1,40 @@
+import { FETCHING_DATA } from '../../constants'
+
 const initialState = {
-  name: 'PumpUp'
+  loaded: false,
+  error: undefined,
+  data: {
+    bio: FETCHING_DATA,
+    name: FETCHING_DATA,
+    profileThumbnail: FETCHING_DATA
+  }
+}
+
+const FETCH_USER_DETAILS_SUCCESS = 'FETCH_USER_DETAILS_SUCCESS'
+const fetchUserDetailsSuccess = user => ({
+  type: FETCH_USER_DETAILS_SUCCESS,
+  user
+})
+
+export const getUserDetails = () => {
+  return (dispatch, getState) => {
+    setTimeout(() => {
+      dispatch(
+        fetchUserDetailsSuccess({
+          bio:
+            'This is a long bio which is supposed to be split into multiple lines and also to be made sure that it doesnt go above threee lines',
+          name: 'Suhair Zain',
+          profileThumbnail: 'https://i.ytimg.com/vi/sr_vL2anfXA/maxresdefault.jpg'
+        })
+      )
+    }, 2000)
+  }
 }
 
 export default (state = initialState, action) => {
   switch (action.type) {
+  case FETCH_USER_DETAILS_SUCCESS:
+    return { ...state, data: action.user }
   default:
     return state
   }
